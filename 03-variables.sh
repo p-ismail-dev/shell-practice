1fbc5285c0b8bd57cc16bc025469e2603b8d7358
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 17:11:04 +0530

    need to change

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh #no space between the value and =
+PERSON2=Ramesh
+
+ echo "$PERSON1 ::Hi, how are you?"
+ echo "$PERSON2 :: I am fine,what about you"
+ echo "$PERSON1 :: I am gud,What are you learning"
+ echo "$PERSON2:: I am learning shell"
