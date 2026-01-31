9684d5a9dcab8a37ab52a6dcc537b3b202d96012
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 22:34:18 +0530

    installation

--- 09-installation.sh
+++ 09-installation.sh
@@ -18,3 +18,25 @@ exit1
 else
 echo "installinf nginx....SUCCESS"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+echo "installing mysql.... Failure"
+exit1
+else
+echo "installinf mysql....SUCCESS"
+fi
+
+dnf install nodesjs -y
+
+if [ $? -ne 0 ]; then
+echo "installing nodejs... Failure"
+exit1
+else
+echo "installinf nodejs...SUCCESS"
+fi
+
+
+
+
