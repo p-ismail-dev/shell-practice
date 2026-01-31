d984eac61439d4927a93c3cfb9574117de29b15b
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 22:39:30 +0530

    installation

--- 09-installation.sh
+++ 09-installation.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
 
 echo "Please run the script with root user access"
-exit1
+exit 1
 
 fi
 
@@ -14,7 +14,7 @@ dnf install nginx -y
 
 if [ $? -ne 0 ]; then
 echo "installing nginx.... Failure"
-exit1
+exit 1
 else
 echo "installinf nginx....SUCCESS"
 fi
@@ -23,7 +23,7 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]; then
 echo "installing mysql.... Failure"
-exit1
+exit 1
 else
 echo "installinf mysql....SUCCESS"
 fi
@@ -32,7 +32,7 @@ dnf install nodesjs -y
 
 if [ $? -ne 0 ]; then
 echo "installing nodejs... Failure"
-exit1
+exit 1
 else
 echo "installinf nodejs...SUCCESS"
 fi
