74e91aa367c9a6d9c83f300cbf20613eeb91e072
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 17:58:09 +0530

    need to change

--- 06-variables.sh
+++ 06-variables.sh
@@ -2,11 +2,11 @@
 
 START_TIME=$(date +%s)
 
-echo "script excecuted time::$START_TIME"
+echo "script excecuted at:$START_TIME"
 
 sleep 10
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
-echo "script excuted in:$TOTAL_TIME seconds"
+echo "script excuted in: $TOTAL_TIME seconds"
