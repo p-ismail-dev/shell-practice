9b63bb9ffb7bf8dbae7e89033519c818d1ec004a
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 16:58:54 +0530

    need to change

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-echo "this is the my second script"
-
-echo "I am learning DevOps"
-
+ echo "Suresh ::Hi, how are you?"
+ echo "Ramesh :: I am fine,what about you"
+ echo "Suresh :: I am gud,What are you learning"
+ echo "Ramesh :: I am learning shell"
