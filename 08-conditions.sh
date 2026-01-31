912e47420a041e4b016386b727d8a960a50a71e1
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 19:15:32 +0530

    need to change

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -7,5 +7,5 @@ if [ $NUMBER -gt 20 ]; then
     elif [ $NUMBER -eq 20 ]; then
     echo "Given Number is: $NUMBER equal to 20"
 else
-    echo "Given Number is: $NUMBER is less than 20
+    echo "Given Number is: $NUMBER is less than 20"
     fi
