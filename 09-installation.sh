71951504f0ecafd23c01dfe1124d91ea87e7f4ae
p-ismail <ismailsmile7337@gmail.com>
2026-01-31 22:11:52 +0530

    installation

---
+++ 09-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+
+echo "Please run the script with root user access"
+exit1
+
+fi
+
+echo "installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+echo "installing nginx.... Failure"
+exit1
+else
+echo "installinf nginx....SUCCESS"
+if
