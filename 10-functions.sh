0bb1c6102d02633644ba9ba7a8600898c6cc3218
p-ismail <ismailsmile7337@gmail.com>
2026-02-01 12:29:39 +0530

    parameters

---
+++ 10-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo "Run the script with the root user access"
+exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+         echo "$2... FAILURE"
+         exit 1
+    else
+        echo "$2.... Success"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+
+dnf install nodejs -y
+VALIDATE $? "Insalling nodejs"
